bf91d60af56241392cc4dcf884a8f1c95c8eecd4
halinaPP <galunikipp@gmail.com>
2020-10-11 03:05:15 +0300

    feat:solve brackets task with stack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsLeftArr = [];
+  const bracketsRightArr = [];
+  let currPairEl;
+  for(let symb of str){
+    if(bracketsLeftArr.length === 0 || !currPairEl || symb!==currPairEl){
+      bracketsLeftArr.push(symb);
+
+      if(currPairEl && symb!==currPairEl){
+        bracketsRightArr.push(currPairEl);
+      }
+      currPairEl = bracketsConfig.find((item) => item[0] === symb);
+
+      if(currPairEl === undefined){
+         return false;
+      }else{
+        currPairEl = currPairEl[1];
+      }
+    }else if( symb === currPairEl ){
+      bracketsLeftArr.pop();
+      currPairEl = bracketsRightArr.pop();
+    }
+  }
+  return bracketsLeftArr.length<1 ? true : false;
 }
